20f2fff85a89c5705a597440718ea3edd190efee
김철우 <kimchul8747@daum.net>
2023-05-08 15:03:28 +0900

    Setting (#3) :: react-query 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import "./App.css";
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <></>;
+  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
 }
 
 export default App;
