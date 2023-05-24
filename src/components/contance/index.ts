32db8b6ac5a541bfd23dcbc00c709b69cfb2c4b6
김한비 <gksql2256@dsm.hs.kr>
2023-05-24 23:11:12 +0900

    ✨:: 수해평가 페이지 퍼블리싱

---
+++ src/components/contance/index.ts
@@ -0,0 +1,38 @@
+export const List=[
+  {
+    date: "2023-5-2",
+    grade_class: "2-3",
+    subject: "임베",
+    teacher: "김한비",
+  },
+  {
+    date: "2023-3-22",
+    grade_class: "2-2",
+    subject: "자바",
+    teacher: "김한비",
+  },
+  {
+    date: "2023-9-31",
+    grade_class: "2-1",
+    subject: "한국사",
+    teacher: "김한비",
+  },
+  {
+    date: "2023-8-26",
+    grade_class: "2-2",
+    subject: "영어",
+    teacher: "김한비",
+  },
+  {
+    date: "2023-7-1",
+    grade_class: "2-3",
+    subject: "마제",
+    teacher: "김한비",
+  },
+  {
+    date: "2023-5-2",
+    grade_class: "2-3",
+    subject: "임베",
+    teacher: "김한비",
+  },
+];
