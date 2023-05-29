ba176ce85235dffd2aeb0682cac51b0d31fe860e
김한비 <gksql2256@dsm.hs.kr>
2023-05-29 17:36:52 +0900

    📝 :: 수행평가 페이지 퍼블리싱 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import Performance from "./components/performanCreate";
+import Performance from "./components/performanceAssessment";
+import { ThemeProvider } from "styled-components";
+import theme from "./utils/theme/theme";
+import GlobalStyle from "./style/globalStyle";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-   <Performance/>
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <Performance />
+        <GlobalStyle />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 
