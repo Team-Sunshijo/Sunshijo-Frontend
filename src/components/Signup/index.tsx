956b695c4a92ac260659e1880d3c70dab82ca74b
김철우 <kimchul8747@daum.net>
2023-06-03 03:05:59 +0900

    💄 :: 불필요한 style 제거로 인한 코드 수정

--- src/components/Signup/index.tsx
+++ src/components/Signup/index.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { SignImage } from "../../assets/Signup";
-import theme from "../../utils/theme/theme";
 
 const Signup = () => {
   return (
@@ -10,24 +9,19 @@ const Signup = () => {
           <img src={SignImage} alt="" />
         </IconWrapper>
         <Content>
-          <TitleWrapper>
-            <TitleText>회원가입</TitleText>
-            <TitleLine />
-          </TitleWrapper>
-          <InputWarpper>
-            <p>아이디</p>
-            <Input placeholder="아이디를 입력해주세요." />
-            <p>비밀번호</p>
-            <Input placeholder="비밀번호를 입력해주세요." />
-            <p>이름</p>
-            <Input placeholder="이름을 입력해주세요." />
-            <p>인증코드</p>
-            <Input placeholder="인증코드를 입력해주세요." />
-          </InputWarpper>
-          <LoginBtn>로그인</LoginBtn>
+          <TitleText>회원가입</TitleText>
+          <ContentText>아이디</ContentText>
+          <Input placeholder="아이디를 입력해주세요." />
+          <ContentText>비밀번호</ContentText>
+          <Input placeholder="비밀번호를 입력해주세요." />
+          <ContentText>이름</ContentText>
+          <Input placeholder="이름을 입력해주세요." />
+          <ContentText>인증코드</ContentText>
+          <Input placeholder="인증코드를 입력해주세요." />
+          <SignupBtn>회원가입</SignupBtn>
           <CheckWrapper>
-            <div>이미 계정이 있으신가요? </div>
-            <GoLogin>로그인 하러가기</GoLogin>
+            <CheckText>이미 계정이 있으신가요? </CheckText>
+            <LoginText>로그인 하러가기</LoginText>
           </CheckWrapper>
         </Content>
       </Wrapper>
@@ -53,7 +47,6 @@ const Wrapper = styled.div`
 
 const IconWrapper = styled.div`
   width: 218px;
-  height: 600px;
   background-color: ${({ theme }) => theme.colors.Main};
   border-radius: 16px 0 0 16px;
   display: flex;
@@ -64,46 +57,25 @@ const IconWrapper = styled.div`
 const Content = styled.div`
   width: 509px;
   height: 623px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: auto;
-  margin-bottom: auto;
-  padding: 20px 0;
-  p {
-    width: 288px;
-    height: 15px;
-    font: ${({ theme }) => theme.font.SemiBold16};
-    display: flex;
-    margin: 18px 0;
-  }
-`;
-
-const TitleWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+  padding: 35px 100px;
 `;
 
 const TitleText = styled.div`
-  width: 288px;
+  width: 83px;
   height: 30px;
   font: ${({ theme }) => theme.font.SemiBold24};
-  display: flex;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.Main};
 `;
 
-const TitleLine = styled.div`
-  width: 81px;
-  height: 1px;
-  background-color: ${({ theme }) => theme.colors.Main};
-`;
-
-const InputWarpper = styled.div`
-  width: 288px;
+const ContentText = styled.div`
+  height: 15px;
+  font: ${({ theme }) => theme.font.SemiBold16};
+  margin: 18px 0;
 `;
 
 export const Input = styled.input`
   font: ${({ theme }) => theme.font.Regular14};
-  width: 266px;
+  width: 288px;
   height: 53px;
   border: none;
   border-radius: 6px;
@@ -111,46 +83,34 @@ export const Input = styled.input`
   box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.16);
   border-radius: 6px;
   padding-left: 19px;
-  :focus {
-    outline: none;
-  }
-  &::placeholder {
-    font: ${({ theme }) => theme.font.Regular14};
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+  outline: none;
 `;
 
-const LoginBtn = styled.button`
+const SignupBtn = styled.button`
   width: 288px;
   height: 41px;
   border: none;
   border-radius: 8px;
   background-color: ${({ theme }) => theme.colors.Main};
-  margin-top: 30px;
+  margin: 20px 0 10px 0;
   color: ${({ theme }) => theme.colors.White};
   font: ${({ theme }) => theme.font.SemiBold16};
-
-  :hover {
-    background-color: ${({ theme }) => theme.colors.Main};
-  }
+  cursor: pointer;
 `;
 
 const CheckWrapper = styled.div`
-  width: 195px;
-  height: 14px;
-  display: flex;
-  justify-content: center;
-  margin-top: 15px;
-  div {
-    font: ${({ theme }) => theme.font.Regular12};
-  }
+  width: 200px;
+  margin: 0 auto;
 `;
 
-const GoLogin = styled.div`
+const CheckText = styled.span`
+  font: ${({ theme }) => theme.font.Regular12};
+`;
+
+const LoginText = styled.span`
   font: ${({ theme }) => theme.font.Medium12};
   color: ${({ theme }) => theme.colors.Main};
+  cursor: pointer;
   margin-left: 2px;
 `;
 
