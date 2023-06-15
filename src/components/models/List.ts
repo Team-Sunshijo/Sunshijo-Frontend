1a0139b751046f9f270514fe5799233d8dc82710
Gwonhaeun0908 <djii3386464@gmail.com>
2023-06-15 22:35:48 +0900

    📝 :: 계획서 작성 디테일 페이지 퍼블리싱

--- src/components/models/List.ts
+++ src/components/models/List.ts
@@ -5,3 +5,34 @@ export interface ListProps {
       teacher: string;
 }
 
+export interface PlanWListProps {
+      date: string;
+      reason: string;
+      request_teacher: string;
+      middle_teacher: string;
+}
+
+export interface ReinforcementListProps {
+      date: string;
+      lesson: string;
+      grade_class: string;
+      missing_subjects: string;
+      reinforcement_plan: string;
+      reinforcement_teacher: string;
+}
+
+export interface ClassChangeListProps {
+      date: string;
+      lesson: string;
+      grade_class: string;
+      subject_replace: string;
+      change_date: string;
+      change_lesson: string;
+      change_grade_class: string;
+      replace_subject: string;
+      charge_teacher: string;
+}
+
+
+
+
