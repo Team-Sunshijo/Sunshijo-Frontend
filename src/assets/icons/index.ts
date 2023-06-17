a5974db3f09717e6f6208eb469abb890997fa8a8
김철우 <kimchul8747@daum.net>
2023-06-17 23:43:00 +0900

    📝 :: export문 오타 수정

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,6 +2,6 @@ export { default as Alram } from "./Alram.svg";
 export { default as SignPerson } from "./SignPerson.svg";
 export { default as DropIcon } from "./DropIcon.svg";
 export { default as ChangeIcon } from "./ChangeIcon.svg";
-export { default as ExplainIcon } from "./ExplainIconIcon.svg";
+export { default as ExplainIcon } from "./ExplainIcon.svg";
 export { default as NextIcon } from "./NextIcon.svg";
-export { default as PreviousIcon } from "./PreviusIcon.svg";
+export { default as PreviousIcon } from "./PreviousIcon.svg";
