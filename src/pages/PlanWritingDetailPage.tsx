7bafed654d2da909b5230b5f5df8f0c42e1d9656
Gwonhaeun0908 <djii3386464@gmail.com>
2023-06-18 22:41:55 +0900

    🔥 :: 중복 파일 삭제

--- src/pages/PlanWritingDetailPage.tsx
+++
@@ -1,7 +0,0 @@
-import PlanWritingDetailList from "../components/planWriting/planWritingDetail";
-
-const PlanWritingDetailPage = () => {
-  return <PlanWritingDetailList />;
-};
-
-export default PlanWritingDetailPage;
