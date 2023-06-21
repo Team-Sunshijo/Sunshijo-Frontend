88a8101d1a5f84ae11eb297688e5d50ee2d7872f
김한비 <gksql2256@dsm.hs.kr>
2023-06-21 22:19:28 +0900

    📝 :: 캘린더 아이콘 코드 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -5,3 +5,5 @@ export { default as ChangeIcon } from "./ChangeIcon.svg";
 export { default as ExplainIcon } from "./ExplainIcon.svg";
 export { default as NextIcon } from "./NextIcon.svg";
 export { default as PreviousIcon } from "./PreviousIcon.svg";
+export { default as CalendarIcon } from "./CalendarIcon.svg";
+
